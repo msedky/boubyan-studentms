042ebd23053f73ec53fe72dd2b68a4c8686ef435
msedky <m.hosamsedky@hotmail.com>
2024-10-01 20:36:33 +0300

    applied weblogic to project

--- src/main/java/org/boubyan/studentms/StudentManagementSystemApplication.java
+++ src/main/java/org/boubyan/studentms/StudentManagementSystemApplication.java
@@ -2,9 +2,10 @@ package org.boubyan.studentms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class StudentManagementSystemApplication {
+public class StudentManagementSystemApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagementSystemApplication.class, args);
