5153fc502ab1d2099e98c4d06237587203f21f0b
msedky <m.hosamsedky@hotmail.com>
2024-10-03 00:10:05 +0300

    no comment

--- src/main/java/org/boubyan/studentms/model/dtos/request/CreateCourseRequestDto.java
+++ src/main/java/org/boubyan/studentms/model/dtos/request/CreateCourseRequestDto.java
@@ -1,7 +1,7 @@
 package org.boubyan.studentms.model.dtos.request;
 
-import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 import lombok.Data;
 
@@ -9,9 +9,9 @@ import lombok.Data;
 public class CreateCourseRequestDto {
 
 	@NotBlank(message = "Course code is required")
-	@Max(value = 10, message = "Course code value cannot exceed 10 characters")
+	@Size(max = 10, message = "Course code value cannot exceed 10 characters")
 	private String code;
 	@NotBlank(message = "Course name is required")
-	@Max(value = 50, message = "Course name value cannot exceed 50 characters")
+	@Size(max = 50, message = "Course name value cannot exceed 50 characters")
 	private String name;
 }
--- src/main/java/org/boubyan/studentms/model/dtos/request/ViewCoursesRequestDto.java
+++ src/main/java/org/boubyan/studentms/model/dtos/request/ViewCoursesRequestDto.java
@@ -2,7 +2,7 @@ package org.boubyan.studentms.model.dtos.request;
 
 import java.io.Serializable;
 
-import javax.validation.constraints.Max;
+import javax.validation.constraints.Size;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -12,10 +12,10 @@ import lombok.EqualsAndHashCode;
 public class ViewCoursesRequestDto implements Serializable {
 
 	private static final long serialVersionUID = 8591663295640438650L;
-	
+
 	private Integer id;
-	@Max(value = 10, message = "Course code value cannot exceed 10 characters")
+	@Size(max = 10, message = "Course code value cannot exceed 10 characters")
 	private String code;
-	@Max(value = 50, message = "Course name value cannot exceed 50 characters")
+	@Size(max = 50, message = "Course name value cannot exceed 50 characters")
 	private String name;
 }
