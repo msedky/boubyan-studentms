629321f85e94b6cb205efb793a4b8302ccd5e25b
msedky <m.hosamsedky@hotmail.com>
2024-10-02 09:04:06 +0300

    let ViewCoursesRequestDto serializable and added @EqualsAndHashCode

--- src/main/java/org/boubyan/studentms/model/dtos/request/ViewCoursesRequestDto.java
+++ src/main/java/org/boubyan/studentms/model/dtos/request/ViewCoursesRequestDto.java
@@ -1,10 +1,16 @@
 package org.boubyan.studentms.model.dtos.request;
 
+import java.io.Serializable;
+
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
-public class ViewCoursesRequestDto {
+@EqualsAndHashCode
+public class ViewCoursesRequestDto implements Serializable {
 
+	private static final long serialVersionUID = 8591663295640438650L;
+	
 	private Integer id;
 	private String code;
 	private String name;
